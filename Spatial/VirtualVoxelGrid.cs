13d924ee8ef2c3362e7e6e5f2f0c8282f26a2ef9
Fraser Waters <frassle@gmail.com>
2012-09-01 14:11:24 +0100

    RLE compressed voxels.

--- Spatial/VirtualVoxelGrid.cs
+++ Spatial/VirtualVoxelGrid.cs
@@ -8,17 +8,17 @@ using System.Threading.Tasks;
 
 namespace Ibasa.Spatial
 {
-    public sealed class VirtualVoxelGrid<T> where T : struct
+    public sealed class VirtualVoxelGrid<T> where T : IEquatable<T>
     {
         struct Page
         {
-            public Page(VoxelGrid<T> voxels, long timestamp)
+            public Page(VoxelChunk<T> voxels, long timestamp)
             {
                 Voxels = voxels;
                 Timestamp = timestamp;
             }
 
-            public VoxelGrid<T> Voxels;
+            public VoxelChunk<T> Voxels;
             public long Timestamp;
         }
 
@@ -39,100 +39,33 @@ namespace Ibasa.Spatial
             get
             {
                 var chunk = GetChunk(point);
-                return chunk.Result[point];
+                return chunk[point];
             }
             set
             {
                 var chunk = GetChunk(point);
-                chunk.Result[point] = value;
+                chunk[point] = value;
             }
         }
 
-        public Task<VoxelGrid<T>> GetChunk(Point3l point)
+        public VoxelChunk<T> GetChunk(Point3l point)
         {
             Point3l chunk = new Point3l(
                 Functions.Divide(point.X, PageSize.Width) * PageSize.Width,
                 Functions.Divide(point.Y, PageSize.Height) * PageSize.Height,
                 Functions.Divide(point.Z, PageSize.Depth) * PageSize.Depth);
 
-            var pagein = PageIn(chunk);
-            return pagein.ContinueWith<VoxelGrid<T>>(task =>
-                {
-                    Page page;
-                    Pages.TryGetValue(chunk, out page);
-                    page.Timestamp = Timestamp++;
-                    return page.Voxels;
-                });
-        }
-
-        private Task PageOut(Point3l chunk)
-        {
-            return Task.Factory.StartNew(() =>
-                {
-                    Page page;
-                    if (Pages.TryGetValue(chunk, out page))
-                    {
-                        Ibasa.IO.BinaryWriter writer = new IO.BinaryWriter(System.IO.File.OpenWrite(chunk.ToString() + ".voxels"));
-                        var voxels = page.Voxels;
-
-                        for (long z = voxels.Bounds.Front; z < voxels.Bounds.Back; ++z)
-                        {
-                            for (long x = voxels.Bounds.Left; x < voxels.Bounds.Right; ++x)
-                            {
-                                for (long y = voxels.Bounds.Bottom; y < voxels.Bounds.Top; ++y)
-                                {
-                                    writer.Write<T>(voxels[new Point3l(chunk.X + x, chunk.Y + y, chunk.Z + z)]);
-                                }
-                            }
-                        }
-
-                        writer.Close();
-
-                        Pages.Remove(chunk);
-                    }
-                });
-        }
-
-        private Task PageIn(Point3l chunk)
-        {
-            return Task.Factory.StartNew(() =>
-                {
-                    Page page;
-                    if (!Pages.TryGetValue(chunk, out page))
-                    {
-                        if (Pages.Count > Residency)
-                        {
-                            PageOut(Pages.OrderBy(pair => pair.Value.Timestamp).First().Key).Wait();
-                        }
-                        else
-                        {
-                            var voxels = new VoxelGrid<T>(new Boxl(chunk, PageSize));
-
-                            try
-                            {
-                                Ibasa.IO.BinaryReader reader = new IO.BinaryReader(System.IO.File.OpenRead(chunk.ToString() + ".voxels"));
-
-                                for (long z = voxels.Bounds.Front; z < voxels.Bounds.Back; ++z)
-                                {
-                                    for (long x = voxels.Bounds.Left; x < voxels.Bounds.Right; ++x)
-                                    {
-                                        for (long y = voxels.Bounds.Bottom; y < voxels.Bounds.Top; ++y)
-                                        {
-                                            voxels[new Point3l(chunk.X + x, chunk.Y + y, chunk.Z + z)] = reader.Read<T>();
-                                        }
-                                    }
-                                }
-
-                                reader.Close();
-                            }
-                            catch (System.IO.FileNotFoundException)
-                            {
-                            }
-
-                            Pages.Add(chunk, new Page(voxels, Timestamp++));
-                        }
-                    }
-                });
+            Page page;
+            if (Pages.TryGetValue(chunk, out page))
+            {
+                return page.Voxels;
+            }
+            else
+            {
+                page = new Page(new VoxelChunk<T>(new Boxl(chunk, PageSize)), Timestamp);
+                Pages.Add(chunk, page);
+                return page.Voxels;
+            }
         }
     }
 }
---
+++ Spatial/VoxelChunk.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ibasa.Numerics.Geometry;
+
+namespace Ibasa.Spatial
+{
+    public sealed class VoxelChunk<T> where T : IEquatable<T>
+    {
+        public struct RLE
+        {
+            public RLE(byte length, T voxel)
+            {
+                Length = length;
+                Voxel = voxel;
+            }
+
+            public byte Length;
+            public T Voxel;
+        }
+
+        public Boxl Bounds { get; private set; }
+        public List<RLE> Compressed { get; private set; }
+        public T[] Uncompressed { get; private set; }
+        public bool IsUncompressed { get { return Uncompressed != null; } }
+
+        public VoxelChunk(Boxl bounds)
+        {
+            Bounds = bounds;
+            Compressed = new List<RLE>();
+            Uncompressed = null;
+            Fill(default(T));
+        }
+
+        public void Fill(T voxel)
+        {
+            if (IsUncompressed)
+            {
+                Compressed.Clear();
+                for (int i = 0; i < Uncompressed.Length; ++i)
+                {
+                    Uncompressed[i] = voxel;
+                }
+            }
+            else
+            {
+                Compressed.Clear();
+                long size = Bounds.Width * Bounds.Height * Bounds.Depth;
+                while(size > 0)
+                {
+                    long length = Math.Min(size, byte.MaxValue + 1);
+                    size -= length;
+                    Compressed.Add(new RLE((byte)(length - 1), voxel));
+                }
+            }
+        }
+
+        public void Compress()
+        {
+            if (Compressed.Count == 0)
+            {
+                RLE rle = new RLE(0, Uncompressed[0]);
+
+                for (int i = 1; i < Uncompressed.Length; ++i)
+                {
+                    if (Uncompressed[i].Equals(rle.Voxel) && rle.Length != byte.MaxValue)
+                    {
+                        rle.Length++;
+                    }
+                    else
+                    {
+                        Compressed.Add(rle);
+                        rle.Voxel = Uncompressed[i];
+                        rle.Length = 0;
+                    }
+                }
+
+                Compressed.Add(rle);
+            }
+        }
+
+        public void Uncompress()
+        {
+            if (!IsUncompressed)
+            {
+                Uncompressed = new T[Bounds.Width * Bounds.Height * Bounds.Depth];
+
+                int offset = 0;
+
+                for (int i = 0; i < Compressed.Count; ++i)
+                {
+                    int length = Compressed[i].Length + 1;
+                    T voxel = Compressed[i].Voxel;
+
+                    for (int j = 0; j < length; ++j)
+                    {
+                        Uncompressed[offset++] = voxel;
+                    }
+                }
+
+                System.Diagnostics.Debug.Assert(offset == Uncompressed.Length);
+            }
+        }
+
+        public T Get(Point3l point)
+        {
+            point = new Point3l(point.X - Bounds.X, point.Y - Bounds.Y, point.Z - Bounds.Z);
+            long index = point.Y + point.X * Bounds.Height + point.Z * Bounds.Height * Bounds.Width;
+            return Uncompressed[index];
+        }
+
+        public void Set(Point3l point, T value)
+        {
+            point = new Point3l(point.X - Bounds.X, point.Y - Bounds.Y, point.Z - Bounds.Z);
+            long index = point.Y + point.X * Bounds.Height + point.Z * Bounds.Height * Bounds.Width;
+            Uncompressed[index] = value;
+            Compressed.Clear();
+        }
+
+        public T this[Point3l point]
+        {
+            get
+            {
+                return Get(point);
+            }
+            set
+            {
+                Set(point, value);
+            }
+        }
+    }
+}
