20e5ee2ab289ee8f1ac2b1f9f19ce3388f3e2be4
Fraser Waters <frassle@gmail.com>
2012-09-09 16:00:56 +0100

    Remove Chunks from VoxelGrid, temporary fix.

--- Spatial/VirtualVoxelGrid.cs
+++ Spatial/VirtualVoxelGrid.cs
@@ -10,23 +10,25 @@ namespace Ibasa.Spatial
 {
     public sealed class VirtualVoxelGrid<T> where T : IEquatable<T>
     {
-        struct Page
+        class Page
         {
-            public Page(VoxelChunk<T> voxels, long timestamp)
+            public Page(VoxelChunk<T> voxels, ulong timestamp)
             {
                 Voxels = voxels;
                 Timestamp = timestamp;
             }
 
             public VoxelChunk<T> Voxels;
-            public long Timestamp;
+            public ulong Timestamp;
         }
 
         public Size3l PageSize { get; private set; }
         public int Residency { get; set; }
 
         Dictionary<Point3l, Page> Pages = new Dictionary<Point3l, Page>();
-        long Timestamp = 0;
+        ulong Timestamp = 0;
+        Page LastPage;
+        Point3l LastChunk = new Point3l(long.MaxValue, long.MaxValue, long.MaxValue);
 
         public VirtualVoxelGrid(int pageSize = 16)
         {
@@ -55,15 +57,41 @@ namespace Ibasa.Spatial
                 Functions.Divide(point.Y, PageSize.Height) * PageSize.Height,
                 Functions.Divide(point.Z, PageSize.Depth) * PageSize.Depth);
 
+            if (LastChunk == chunk)
+            {
+                return LastPage.Voxels;
+            }
+            LastChunk = chunk;
+
             Page page;
             if (Pages.TryGetValue(chunk, out page))
             {
+                page.Timestamp++;
+
+                LastPage = page;
                 return page.Voxels;
             }
             else
             {
-                page = new Page(new VoxelChunk<T>(new Boxl(chunk, PageSize)), Timestamp);
+                page = new Page(new VoxelChunk<T>(new Boxl(chunk, PageSize)), Timestamp++);
                 Pages.Add(chunk, page);
+
+                if (Pages.Count >= 8 * 8 * 8)
+                {
+                    Point3l key = Point3l.Zero;
+                    var timestamp = ulong.MaxValue;
+                    foreach (var pair in Pages)
+                    {
+                        if (pair.Value.Timestamp <= timestamp)
+                        {
+                            timestamp = pair.Value.Timestamp;
+                            key = pair.Key;
+                        }
+                    }
+                    Pages.Remove(key);
+                }
+
+                LastPage = page;
                 return page.Voxels;
             }
         }
