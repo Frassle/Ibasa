c3e9923150779e6212e6f8538357f9112f589383
Fraser Waters <frassle@gmail.com>
2013-01-16 23:54:06 +0000

    StreamExtension methods, ReadBytes.

---
+++ Ibasa/IO/StreamExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ibasa.IO
+{
+    public static class StreamExtensions
+    {
+        public static byte[] ReadBytes(this System.IO.Stream stream, int count)
+        {
+            var buffer = new byte[count];
+            ReadBytes(stream, buffer, 0, count);
+            return buffer;
+        }
+
+        public static void ReadBytes(this System.IO.Stream stream, byte[] buffer, int offset, int count)
+        {
+            int bytes_left = count;
+
+            do
+            {
+                int read = stream.Read(buffer, offset, bytes_left);
+                if (read == 0)
+                    break;
+                offset += read;
+                bytes_left -= read;
+            } while (bytes_left > 0);
+
+            if (bytes_left != 0)
+                throw new System.IO.EndOfStreamException(string.Format("Could not read {0} bytes.", count));
+        }
+    }
+}
