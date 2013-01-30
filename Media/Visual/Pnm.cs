e9d12fe7a057af45c6214209fadc1f6e91155e9b
Fraser Waters <frassle@gmail.com>
2013-01-30 14:20:09 +0000

    Added support for float images.

--- Media/Visual/Pnm.cs
+++ Media/Visual/Pnm.cs
@@ -22,9 +22,11 @@ namespace Ibasa.Media.Visual
 
                 if (!(
                     value.Format is SharpIL.Formats.R8G8B8UNorm ||
-                    value.Format is SharpIL.Formats.R8UNorm))
+                    value.Format is SharpIL.Formats.R8UNorm ||
+                    value.Format is SharpIL.Formats.R32G32B32Float ||
+                    value.Format is SharpIL.Formats.R32Float))
                     throw new ArgumentException(
-                        "Unsupported format. Pnm only supports R8G8B8 and R8.",
+                        "Unsupported format. Pnm only supports R8G8B8, R8, R32G32B32 and R32.",
                         "value");
 
                 if (value.Size.Depth > 1)
@@ -67,9 +69,23 @@ namespace Ibasa.Media.Visual
             if (pByte != 'P')
                 throw new InvalidDataException();
 
-            int type = iByte - '0';
 
-            if (type < 1 || type > 6)
+            int type;
+
+            if (iByte == 'f')
+            {
+                type = 7;
+            }
+            else if (iByte == 'F')
+            {
+                type = 8;
+            }
+            else
+            {
+                type = iByte - '0';
+            }
+
+            if (type < 1 || type > 8)
                 throw new InvalidDataException();
 
             BinaryReader reader = new BinaryReader(stream, Encoding.ASCII);
@@ -78,27 +94,39 @@ namespace Ibasa.Media.Visual
             int max = 0;
             if (type != 1 && type != 4)
                 max = Parse(reader);
+            int scale = 0;
+            if (type == 7 || type == 8)
+                scale = Parse(reader);
+
+            if (scale < 0)
+                throw new NotSupportedException("Does not support big endian floats.");
 
             switch (type)
             {
-                case 1: //Bitmap
+                case 1: // Bitmap
                     ImageData = ReadASCIIBitmap(reader, width, height);
                     break;
-                case 2: //Graymap
+                case 2: // Graymap
                     ImageData = ReadASCIIGraymap(reader, width, height, max);
                     break;
-                case 3: //Pixmap
+                case 3: // Pixmap
                     ImageData = ReadASCIIPixmap(reader, width, height, max);
                     break;
                 //Binary
-                case 4: //Bitmap
+                case 4: // Bitmap
                     ImageData = ReadBinaryBitmap(reader, width, height);
                     break;
-                case 5: //Graymap
+                case 5: // Graymap
                     ImageData = ReadBinaryGraymap(reader, width, height, max);
                     break;
-                case 6: //Pixmap
+                case 6: // Pixmap
                     ImageData = ReadBinaryPixmap(reader, width, height, max);
+                    break;
+                case 7: // Float graymap
+                    ImageData = ReadBinaryFloatGraymap(reader, width, height, scale);
+                    break;
+                case 8: // Float Pixmap
+                    ImageData = ReadBinaryFloatPixmap(reader, width, height, scale);
                     break;
             }
         }
@@ -202,6 +230,42 @@ namespace Ibasa.Media.Visual
             return image;
         }
 
+        private Resource ReadBinaryFloatGraymap(BinaryReader reader, int width, int height, int scale)
+        {
+            Resource image = new Resource(new Size3i(width, height, 1), 1, 1, Format.R32Float);
+            byte[] data = image[0, 0];
+
+            int c = reader.PeekChar();
+            if (c == -1 || char.IsWhiteSpace((char)c))
+                reader.ReadChar();
+
+            for (int i = 0; i < data.Length; i += 4)
+            {
+                BitConverter.GetBytes(data, i, reader.ReadSingle() * scale);
+            }
+
+            return image;
+        }
+
+        private Resource ReadBinaryFloatPixmap(BinaryReader reader, int width, int height, int scale)
+        {
+            Resource image = new Resource(new Size3i(width, height, 1), 1, 1, Format.R32G32B32Float);
+            byte[] data = image[0, 0];
+
+            int c = reader.PeekChar();
+            if (c == -1 || char.IsWhiteSpace((char)c))
+                reader.ReadChar();
+
+            for (int i = 0; i < data.Length; i += 12)
+            {
+                BitConverter.GetBytes(data, i + 0, reader.ReadSingle() * scale);
+                BitConverter.GetBytes(data, i + 4, reader.ReadSingle() * scale);
+                BitConverter.GetBytes(data, i + 8, reader.ReadSingle() * scale);
+            }
+
+            return image;
+        }
+
         private int Parse(BinaryReader reader)
         {
             while (char.IsWhiteSpace(reader.ReadChar()))
@@ -215,15 +279,26 @@ namespace Ibasa.Media.Visual
             }
 
             int result = 0;
+            bool negative = false;
 
-            char c;
-            while (char.IsDigit(c = reader.ReadChar()))
+            char c = reader.ReadChar();
+            if (c == '+')
+            {
+                c = reader.ReadChar();
+            }
+            else if (c == '-')
+            {
+                negative = true;
+                c = reader.ReadChar();
+            }
+
+            for(; char.IsDigit(c); c = reader.ReadChar())
             {
                 result *= 10;
                 result += (c - '0');
             }
 
-            return result;
+            return negative ? -result : result;
         }
 
         public void Save(string path, bool ascii)
@@ -239,8 +314,13 @@ namespace Ibasa.Media.Visual
 
             writer.Write('P');
 
+            bool floating = Image.Format is SharpIL.Formats.R32G32B32Float || Image.Format is SharpIL.Formats.R32Float;
+
             if (ascii)
             {
+                if (floating)
+                    throw new ArgumentException("Cannot save float image as ASCII", "ascii");
+
                 if (Image.Format is SharpIL.Formats.R8G8B8UNorm)
                     writer.Write('3');
                 else
@@ -250,8 +330,12 @@ namespace Ibasa.Media.Visual
             {
                 if (Image.Format is SharpIL.Formats.R8G8B8UNorm)
                     writer.Write('6');
-                else
+                else if(Image.Format is SharpIL.Formats.R8UNorm)
                     writer.Write('5');
+                else if (Image.Format is SharpIL.Formats.R32G32B32Float)
+                    writer.Write('F');
+                else if (Image.Format is SharpIL.Formats.R32Float)
+                    writer.Write('f');
             }
 
             writer.Write(Environment.NewLine);
@@ -259,7 +343,7 @@ namespace Ibasa.Media.Visual
             writer.Write(" ");
             writer.Write(Image.Size.Height.ToString());
             writer.Write(Environment.NewLine);
-            writer.Write("255");
+            writer.Write(floating ? "1" : "255");
             writer.Write(Environment.NewLine);
 
             byte[] data = Image[0, 0];
@@ -301,32 +385,7 @@ namespace Ibasa.Media.Visual
             }
             else
             {
-                if (Image.Format is SharpIL.Formats.R8G8B8UNorm)
-                {
-                    for (int y = Image.Size.Height - 1; y >= 0; --y)
-                    {
-                        for (int x = 0; x < Image.Size.Width; ++x)
-                        {
-                            int offset = (3 * x) + (y * 3 * Image.Size.Width);
-
-                            writer.Write(data[offset + 0]);
-                            writer.Write(data[offset + 1]);
-                            writer.Write(data[offset + 2]);
-                        }
-                    }
-                }
-                else
-                {
-                    for (int y = Image.Size.Height - 1; y >= 0; --y)
-                    {
-                        for (int x = 0; x < Image.Size.Width; ++x)
-                        {
-                            int offset = x + y * Image.Size.Width;
-
-                            writer.Write(data[offset]);
-                        }
-                    }
-                }
+                writer.Write(data, 0, data.Length);
             }
 
             writer.Close();
