6a142d4da1b89aee48c8afc520f1c57a7ce20405
Fraser <frassle@gmail.com>
2013-04-10 23:27:05 +0100

    Memory and event enums.

--- OpenCL/Enums.cs
+++ OpenCL/Enums.cs
@@ -24,4 +24,37 @@ namespace Ibasa.OpenCL
         OutOfOrderExecModeEnable = CL.QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE,
         ProfilingEnable = CL.QUEUE_PROFILING_ENABLE,
     }
+
+    /// <summary>
+    /// A bit-field that is used to specify allocation and usage information
+    /// such as the memory arena that should be used to allocate the buffer 
+    /// object and how it will be used. The following table describes the 
+    /// possible values for flags. If value specified for flags is 0, the 
+    /// default is used which is ReadWrite.
+    /// </summary>
+    [Flags]
+    public enum MemFlags
+    {
+        /// <summary>
+        /// This flag specifies that the memory object will be read and 
+        /// written by a kernel. This is the default.
+        /// </summary>
+        ReadWrite = CL.MEM_READ_WRITE,
+        WriteOnly = CL.MEM_WRITE_ONLY,
+        ReadOnly = CL.MEM_READ_ONLY,
+        UseHostPtr = CL.MEM_USE_HOST_PTR,
+        AllocHostPtr = CL.MEM_ALLOC_HOST_PTR,
+        CopyHostPtr = CL.MEM_COPY_HOST_PTR,
+        HostWriteOnly = CL.MEM_HOST_WRITE_ONLY,
+        HostReadOnly = CL.MEM_HOST_READ_ONLY,
+        HostNoAccess = CL.MEM_HOST_NO_ACCESS,
+    }
+
+    public enum CommandExecutionStatus
+    {
+        Complete = CL.COMPLETE,
+        Running = CL.RUNNING,
+        Submitted = CL.SUBMITTED,
+        Queued = CL.QUEUED,
+    }
 }
