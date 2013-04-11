ec0849f413cbba8f771537ab45d686d23a947560
Fraser <frassle@gmail.com>
2013-04-11 04:02:32 +0100

    Kernel properties

--- OpenCL/Kernel.cs
+++ OpenCL/Kernel.cs
@@ -61,6 +61,189 @@ namespace Ibasa.OpenCL
             CLHelper.ThrowNullException(Handle);
             CLHelper.GetError(CL.ReleaseKernel(Handle));
         }
+
+        public void SetArgument(int index, int size, byte[] value)
+        {
+        }
+
+        public string FunctionName
+        {
+            get
+            {
+                CLHelper.ThrowNullException(Handle);
+                unsafe
+                {
+                    UIntPtr param_value_size_ret = UIntPtr.Zero;
+                    CLHelper.GetError(CL.GetKernelInfo(
+                        Handle, CL.KERNEL_FUNCTION_NAME, UIntPtr.Zero, null, &param_value_size_ret));
+
+                    byte* data_ptr = stackalloc byte[(int)param_value_size_ret.ToUInt32()];
+
+                    CLHelper.GetError(CL.GetKernelInfo(
+                        Handle, CL.KERNEL_FUNCTION_NAME, param_value_size_ret, data_ptr, null));
+
+                    return Marshal.PtrToStringAnsi(new IntPtr(data_ptr), (int)param_value_size_ret.ToUInt32() - 1);
+                }
+            }
+        }
+
+        public long ArgumentCount
+        {
+            get
+            {
+                CLHelper.ThrowNullException(Handle);
+                unsafe
+                {
+                    uint value;
+                    UIntPtr param_value_size = new UIntPtr(sizeof(uint));
+                    CLHelper.GetError(CL.GetKernelInfo(
+                        Handle, CL.KERNEL_NUM_ARGS, param_value_size, &value, null));
+                    return value;
+                }
+            }
+        }
+
+        public long ReferenceCount
+        {
+            get
+            {
+                CLHelper.ThrowNullException(Handle);
+                unsafe
+                {
+                    uint value;
+                    UIntPtr param_value_size = new UIntPtr(sizeof(uint));
+                    CLHelper.GetError(CL.GetKernelInfo(
+                        Handle, CL.KERNEL_REFERENCE_COUNT, param_value_size, &value, null));
+                    return value;
+                }
+            }
+        }
+
+        Context Context
+        {
+            get
+            {
+                CLHelper.ThrowNullException(Handle);
+                unsafe
+                {
+                    IntPtr value;
+                    UIntPtr param_value_size = new UIntPtr((uint)IntPtr.Size);
+                    CLHelper.GetError(CL.GetKernelInfo(
+                        Handle, CL.KERNEL_CONTEXT, param_value_size, &value, null));
+                    return new Context(value);
+                }
+            }
+        }
+
+        Program Program
+        {
+            get
+            {
+                CLHelper.ThrowNullException(Handle);
+                unsafe
+                {
+                    IntPtr value;
+                    UIntPtr param_value_size = new UIntPtr((uint)IntPtr.Size);
+                    CLHelper.GetError(CL.GetKernelInfo(
+                        Handle, CL.KERNEL_PROGRAM, param_value_size, &value, null));
+                    return new Program(value);
+                }
+            }
+        }
+
+        public string Attributes
+        {
+            get
+            {
+                CLHelper.ThrowNullException(Handle);
+                unsafe
+                {
+                    UIntPtr param_value_size_ret = UIntPtr.Zero;
+                    CLHelper.GetError(CL.GetKernelInfo(
+                        Handle, CL.KERNEL_ATTRIBUTES, UIntPtr.Zero, null, &param_value_size_ret));
+
+                    byte* data_ptr = stackalloc byte[(int)param_value_size_ret.ToUInt32()];
+
+                    CLHelper.GetError(CL.GetKernelInfo(
+                        Handle, CL.KERNEL_ATTRIBUTES, param_value_size_ret, data_ptr, null));
+
+                    return Marshal.PtrToStringAnsi(new IntPtr(data_ptr), (int)param_value_size_ret.ToUInt32() - 1);
+                }
+            }
+        }
+
+        public struct WorkGroupInfo
+        {
+            IntPtr Kernel;
+            IntPtr Device;
+
+            internal WorkGroupInfo(IntPtr kernel, IntPtr device)
+            {
+                Kernel = kernel;
+                Device = device;
+            }
+
+            public ulong WorkGroupSize
+            {
+                get
+                {
+                    CLHelper.ThrowNullException(Kernel);
+                    unsafe
+                    {
+                        UIntPtr value;
+                        UIntPtr param_value_size = new UIntPtr((uint)UIntPtr.Size);
+                        CLHelper.GetError(CL.GetKernelWorkGroupInfo(Kernel, Device,
+                            CL.KERNEL_WORK_GROUP_SIZE, param_value_size, &value, null));
+                        return value.ToUInt64();
+                    }
+                }
+            }
+
+            public ulong[] CompileWorkGroupSize
+            {
+                get
+                {
+                    CLHelper.ThrowNullException(Kernel);
+                    unsafe
+                    {
+                        UIntPtr* value = stackalloc UIntPtr[3];
+                        UIntPtr param_value_size = new UIntPtr((uint)UIntPtr.Size * 3);
+                        CLHelper.GetError(CL.GetKernelWorkGroupInfo(Kernel, Device,
+                            CL.KERNEL_COMPILE_WORK_GROUP_SIZE, param_value_size, value, null));
+
+                        ulong[] result = new ulong[3];
+                        result[0] = value[0].ToUInt64();
+                        result[1] = value[1].ToUInt64();
+                        result[2] = value[2].ToUInt64();
+                        return result;
+                    }
+                }
+            }
+
+            public ulong LocalMemorySize
+            {
+                get
+                {
+                    CLHelper.ThrowNullException(Kernel);
+                    unsafe
+                    {
+                        ulong value;
+                        UIntPtr param_value_size = new UIntPtr(sizeof(ulong));
+                        CLHelper.GetError(CL.GetKernelWorkGroupInfo(Kernel, Device,
+                            CL.KERNEL_LOCAL_MEM_SIZE, param_value_size, &value, null));
+                        return value;
+                    }
+                }
+            }
+        }
+
+        public WorkGroupInfo GetWorkGroupInfo(Device device)
+        {
+            CLHelper.ThrowNullException(Handle);
+            return new WorkGroupInfo(Handle, device.Handle);
+        }
+
+
         public override int GetHashCode()
         {
             CLHelper.ThrowNullException(Handle);
