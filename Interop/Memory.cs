0652f5f535ecafc39f1b0933405b2506fcbcc8da
Fraser <frassle@gmail.com>
2013-04-17 10:33:35 +0100

    Add struct constraint.

--- Interop/Memory.cs
+++ Interop/Memory.cs
@@ -21,17 +21,17 @@ namespace Ibasa.Interop
             throw new NotImplementedException();
         }
 
-        public static unsafe void* Write<T>(void* dst, ref T value)
+        public static unsafe void* Write<T>(void* dst, ref T value) where T : struct
         {
             throw new NotImplementedException();
         }
 
-        public static unsafe void* Read<T>(void* src, out T value)
+        public static unsafe void* Read<T>(void* src, out T value) where T : struct
         {
             throw new NotImplementedException();
         }
 
-        public static unsafe int SizeOf<T>()
+        public static unsafe int SizeOf<T>() where T : struct
         {
             throw new NotImplementedException();
         }
@@ -46,12 +46,12 @@ namespace Ibasa.Interop
             Fill(dst.ToPointer(), value, count);
         }
 
-        public static unsafe IntPtr Write<T>(IntPtr dst, ref T value)
+        public static unsafe IntPtr Write<T>(IntPtr dst, ref T value) where T : struct
         {
             return new IntPtr(Write(dst.ToPointer(), ref value));
         }
 
-        public static unsafe IntPtr Read<T>(IntPtr src, out T value)
+        public static unsafe IntPtr Read<T>(IntPtr src, out T value) where T : struct
         {
             return new IntPtr(Read(src.ToPointer(), out value));
         }
